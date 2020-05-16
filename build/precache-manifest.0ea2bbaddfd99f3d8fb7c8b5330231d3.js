2fac31b2976bf1c0c70ddb243ac5dac77b8756fd
Ayesha Ali <56182013+sanaa-sys@users.noreply.github.com>
2020-05-16 21:26:53 +1000

    Add files via upload

---
+++ build/precache-manifest.0ea2bbaddfd99f3d8fb7c8b5330231d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9dc8ed00590ff001b93d8d2de79d7a5",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "5eb1661fcfeb215445b9",
+    "url": "/weather-app-react/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "6a49b3efcc25bee14f10",
+    "url": "/weather-app-react/static/css/main.dd0fe1f8.chunk.css"
+  },
+  {
+    "revision": "5eb1661fcfeb215445b9",
+    "url": "/weather-app-react/static/js/2.9f8ab709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.9f8ab709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a49b3efcc25bee14f10",
+    "url": "/weather-app-react/static/js/main.b1ba2a31.chunk.js"
+  },
+  {
+    "revision": "ecf3ae8c8b1cfd4ec175",
+    "url": "/weather-app-react/static/js/runtime-main.8c31db94.js"
+  },
+  {
+    "revision": "2b8b490c9cf56b9a6b59b23b7e7b4d04",
+    "url": "/weather-app-react/static/media/cold-bg.2b8b490c.jpg"
+  },
+  {
+    "revision": "61fed1f8b7314a15361fbb23ae304829",
+    "url": "/weather-app-react/static/media/warm-bg.61fed1f8.jpg"
+  }
+]);
